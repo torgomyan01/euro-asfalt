62c7173634a69214265bcdc0cc0914b0da00fed7
torgomyan01 <and.torgomyan01@gmail.com>
2026-04-21 20:50:40 +0400

    Add metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import 'swiper/css/pagination';
 import './tailwind.css';
 
 import { Metadata } from 'next';
+import Script from 'next/script';
 import NextTopLoader from 'nextjs-toploader';
 import { Providers } from '@/app/providers';
 
@@ -39,6 +40,33 @@ export default async function RootLayout({
   return (
     <html lang="ru" suppressHydrationWarning={true} className="light">
       <body className="text-foreground bg-background">
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108703433', 'ym');
+
+            ym(108703433, 'init', {
+              ssr: true,
+              webvisor: true,
+              clickmap: true,
+              ecommerce: 'dataLayer',
+              referrer: document.referrer,
+              url: location.href,
+              accurateTrackBounce: true,
+              trackLinks: true
+            });
+          `}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<div><img src="https://mc.yandex.ru/watch/108703433" style="position:absolute; left:-9999px;" alt="" /></div>',
+          }}
+        />
         <NextTopLoader color="#f5a623" showSpinner={false} />
         <Providers>{children}</Providers>
       </body>
